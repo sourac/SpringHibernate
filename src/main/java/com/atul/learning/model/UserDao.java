540ab7f1063069ebd3938391b2713721119c9ba9
atul <atulsharma21772@gmail.com>
2017-06-05 13:33:46 +0530

    added annotation @transactional

--- src/main/java/com/atul/learning/model/UserDao.java
+++ src/main/java/com/atul/learning/model/UserDao.java
@@ -1,7 +1,7 @@
 package com.atul.learning.model;
 
 import org.springframework.data.repository.CrudRepository;
-
+@Transactional
 public interface UserDao extends CrudRepository<User, Long> {
 	
 	public User findByEmail(String email);
