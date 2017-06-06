2cabe0f963b34ffb6df9ebd3828c2d4332b59633
atul <atulsharma21772@gmail.com>
2017-06-06 12:12:52 +0530

    just spacing

--- src/main/java/com/atul/learning/model/UserDao.java
+++ src/main/java/com/atul/learning/model/UserDao.java
@@ -3,10 +3,12 @@ package com.atul.learning.model;
 import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
+
 @Transactional
 public interface UserDao extends CrudRepository<User, Long> {
-	
+
 	public User findByEmail(String email);
+
 	public User findByName(String name);
 
 }
